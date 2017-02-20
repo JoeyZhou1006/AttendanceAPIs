edd336e01d2250e9778ee65938674b028b1a20fa
JoeyZhou1006 <JoeyZhou1006@users.noreply.github.com>
2017-02-20 19:06:35 +1030

    Update getUserImageByGivenName.php

--- api/getUserImageByGivenName.php
+++ api/getUserImageByGivenName.php
@@ -17,14 +17,8 @@ $staff=array("Jennifer Wu","Kenny Chau","Li Wang","Nevi Pitt","Ren Tharakan");
 //a for loop to go through the array
 foreach ($staff as $value) {
 	echo "$value";
-	//then for each staff, we need to query the table to get the image data
+	
 
 }
-
-//getting the image by given name
-
-//get app post data which should be an arrays of all staff names
-
-
 
 ?>
