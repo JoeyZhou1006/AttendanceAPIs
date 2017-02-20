092b692f4ee78dae5f9d85bf2255a51ade4d3b65
JoeyZhou1006 <JoeyZhou1006@users.noreply.github.com>
2017-02-20 19:04:30 +1030

    Update getLastOnSiteInfoByName.php

--- api/getLastOnSiteInfoByName.php
+++ api/getLastOnSiteInfoByName.php
@@ -9,51 +9,18 @@ $response = array();
 //this key will store an array iteself
 $response['LastOnsiteData'] = array();
 
-//$response2['DailyRecord'] = array();
 
-//creating object of class DbOperation
+//creating object of class DbOperation so we can use the data query methods
 $db = new DbOperation();
 
+//get the name of the staff that we want to get the last onsite info from the request
 $postVar = $_GET['Name'];
-//$postVar = 'TishCustance';
 
-//getting the teams using the function we created
+//get the last onsite information of the staff using the sql methods from DbOperation
 $result = $db->getLastRecordOfStaff($postVar);
 
 $temp = array();
 
-//$temp['OnSite'] = (int)$result;
-
-
-
-//$temp = ['Onsite': (int)$result]
-
-//a method should return a image by given na
-//it should return the full url of given user
-
-//looping through all the teams.
-
-    //creating a temporary array
-   
-
-    //inserting the team in the temporary array
- 
-    //$temp['Name']=$user['Name'];
-
-
-
-   // base64_encode(file_get_contents('path/to/image.png'));
-    //$temp['Photo_base64_encoded']= base64_encode("http://10.10.10.72/Test/api/" . $user['Photo']);
-
-    //http://192.168.1.7
-   // $temp['Photo_Url'] = "http://10.10.10.72/Test/AllImages/" . $user['Photo'];
-    //$temp['UserImage']=
-
-
-    //inserting the temporary array inside response
-   // array_push($response['LastOnsiteData'], array('OnSite',(int)$result));
-
-
-//displaying the array in json format, and return the resul to the app
+//displaying the array in json format, and return the result to the app
 echo $result;
 ?>
